bba92a9924f49cf7178946496b583e47a0e3d5fa
John Brainard Dela Cruz <brainardddd@gmail.com>
2026-01-22 22:26:30 +0800

    Added Practices

---
+++ LearningJava/src/Practice/IntroToVariables.java
@@ -0,0 +1,41 @@
+package Practice;
+
+public class IntroToVariables {
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = x + 1;
+		
+		System.out.println("This variable is x: " + x);
+		System.out.println("This variable is y: " + y);
+		
+		
+		// Another example of "int"
+		
+		int i;
+		int j;
+		
+		i = 2;
+		j = i * 2;
+		
+		System.out.println("This variable is i: " + i);
+		System.out.println("This variable is j: " + j);
+		
+		
+		// Another example of "string"
+		
+		String a = "Jebi";
+		String b = "Dana";
+		
+		System.out.println(a + " loves " + b + " so much!");
+		
+		
+		// Another example of "double"
+		
+		double Pie = 3.14;
+		
+		System.out.println(Pie);
+		
+	}
+	
+}
---
+++ LearningJava/src/Practice/PrintingLines.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class PrintingLines {
+	public static void main(String[] args) {
+		System.out.println("Hello There!");
+		System.out.println(2026 + " and I love Dana Requina!");
+		
+		// Testing print and println
+		System.out.println("Hello!");
+		System.out.print("John ");
+		System.out.print("Brainard!");
+		
+		// After and Before the "Hello!" String
+		System.out.println(100 + 100 + "Hello!");
+		System.out.println("Hello!" + 100 + 100);
+	}
+}
